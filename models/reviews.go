647bf35e840f7ce999bfccd6f7b36ed4dc405d32
Luigi <lugminsinlag@gmail.com>
2023-08-04 14:37:53 +0200

    feat: Creating the POST model method for reviews.

--- models/reviews.go
+++ models/reviews.go
@@ -1,2 +1,22 @@
 package models
 
+import "github.com/luigiMinardi/alurachallenge-backend-7/db"
+
+type Review struct {
+    Id      int     `json:"id"`
+    Name    string  `json:"name"`
+    Review  string  `json:"review"`
+    Image   []byte  `json:"image"`
+}
+
+func CreateReview(name, review string, image []byte) {
+    db := db.ConnectWithDB()
+
+    sqlToInsert, err := db.Prepare("insert into reviews(name, review, image) values($1, $2, $3)")
+    if err != nil {
+        panic(err.Error())
+    }
+
+    sqlToInsert.Exec(name, review, image)
+    defer db.Close()
+}
