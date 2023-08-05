cd25e9f30c5d25a5a0fdf125a387dd2bf43fe696
Luigi <lugminsinlag@gmail.com>
2023-08-05 20:33:22 +0200

    feat: Creating EditReview controller.

--- controllers/reviews.go
+++ controllers/reviews.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
-    "encoding/json"
-    "net/http"
-    "github.com/luigiMinardi/alurachallenge-backend-7/models"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/luigiMinardi/alurachallenge-backend-7/models"
 )
 
 func AddReview(w http.ResponseWriter, r *http.Request) {
@@ -20,4 +22,18 @@ func AddReview(w http.ResponseWriter, r *http.Request) {
         panic(err)
     }
     w.Write(response)
+}
+
+func EditReview(w http.ResponseWriter, r *http.Request) {
+    id := r.URL.Query().Get("id")
+
+    idToInt, err := strconv.Atoi(id)
+    if err != nil {
+        panic(err)
+    }
+
+    var data models.Review
+    json.NewDecoder(r.Body).Decode(&data)
+
+    models.UpdateReview(idToInt, data.Name, data.Review, data.Image)
 }
