76cedc8bde80a88724e493394374d7e94b68086b
Luigi <lugminsinlag@gmail.com>
2023-08-05 09:01:21 +0200

    chore: cleaning code

--- controllers/reviews.go
+++ controllers/reviews.go
@@ -1,9 +1,7 @@
 package controllers
 
 import (
-    //"encoding/base64"
     "encoding/json"
-    "log"
     "net/http"
     "github.com/luigiMinardi/alurachallenge-backend-7/models"
 )
@@ -11,12 +9,6 @@ import (
 func AddReview(w http.ResponseWriter, r *http.Request) {
     var data models.Review
     json.NewDecoder(r.Body).Decode(&data)
-    /*
-    decodedImage, err := base64.StdEncoding.DecodeString(data.Image)
-    if err != nil {
-        panic(err)
-    }
-    */
     models.CreateReview(data.Name, data.Review, data.Image)
 
     w.WriteHeader(http.StatusOK)
@@ -25,7 +17,7 @@ func AddReview(w http.ResponseWriter, r *http.Request) {
     resp["message"] = "Everything worked as expected"
     response, err := json.Marshal(resp)
     if err != nil {
-        log.Panic(err)
+        panic(err)
     }
     w.Write(response)
 }
--- models/reviews.go
+++ models/reviews.go
@@ -13,12 +13,8 @@ type Review struct {
 
 func CreateReview(name string, review string, image string) {
     db := db.ConnectWithDB()
-    err := db.Ping()
-    if err != nil {
-        panic(err)
-    }
 
-    _, err = db.Exec("insert into reviews(name, review, image) values($1, $2, $3)", name, review, image)
+    _, err := db.Exec("insert into reviews(name, review, image) values($1, $2, $3)", name, review, image)
     if err != nil {
         panic(err)
     }
