582d9bacfcdc89d64641a50f5d426352af52d61a
Luigi <lugminsinlag@gmail.com>
2023-08-05 20:20:14 +0200

    feat: Creating SQL model to update a review.

--- models/reviews.go
+++ models/reviews.go
@@ -11,7 +11,7 @@ type Review struct {
     Image   string  `json:"image"`
 }
 
-func CreateReview(name string, review string, image string) {
+func CreateReview(name, review string, image string) {
     db := db.ConnectWithDB()
 
     _, err := db.Exec("insert into reviews(name, review, image) values($1, $2, $3)", name, review, image)
@@ -19,6 +19,16 @@ func CreateReview(name string, review string, image string) {
         panic(err)
     }
 
-    
+    defer db.Close()
+}
+
+func UpdateReview(id int, name, review string, image string) {
+    db := db.ConnectWithDB()
+
+    _, err := db.Exec("update products set name=$1, review=$2, image=$3 where id=$4", name, review, image, id)
+    if err != nil {
+        panic(err)
+    }
+
     defer db.Close()
 }
